57ad1fc7b87b8783e992bc6a79fb981671c02df7
ashishbansal1975 <ashish_bansal01@yahoo.com>
2016-08-15 17:51:58 +0530

    Hi

---
+++ Session7Assignment2.java
@@ -0,0 +1,17 @@
+public class Session7Assignment2
+{
+    public static void main(String [] args) 
+    {
+       		
+        try
+        {
+	      int num=Integer.parseInt ("XYZ") ;
+	      System.out.println(num);
+        }
+
+       catch(NumberFormatException e)
+        {
+	        System.out.println("Number format exception occurred : "+ e);
+        }
+    }
+}
